5ef05aa2e4091265be077374d6a5f68f7c6ec845
Koji Yamanaga <kouji.yamanaga@daikin.co.jp>
2021-10-09 19:25:05 +0900

    add vue project

---
+++ vue/vue_typescript_test/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <HelloWorld />
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import HelloWorld from "./components/Welcome.vue";
+
+export default defineComponent({
+  name: "App",
+  components: {
+    HelloWorld,
+  },
+});
+
+const add = (a : number, b: number) => a + b;
+console.log(add(1, 3))
+const object = {
+  method1: function() {
+    return this;
+  }
+}
+console.log(object.method1)
+</script>
+
